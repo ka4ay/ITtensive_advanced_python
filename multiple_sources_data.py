931c98d036dbae13eaa0bd1f434f581b22e4d5e2
ka4ay <misha.ka4ay@yandex.ru>
2023-01-07 02:36:50 +0300

    Added file with second task: multiple sources data. Combining data and sorting by index.

---
+++ multiple_sources_data.py
@@ -0,0 +1,22 @@
+'''Получите данные по безработице в Москве:
+https://video.ittensive.com/python-advanced/data-9753-2019-07-25.utf.csv
+Объедините эти данные индексами (Месяц/Год) с данными из предыдущего задания (вызовы пожарных) для Центральный
+административный округ:
+https://video.ittensive.com/python-advanced/data-5283-2019-10-04.utf.csv
+Найдите значение поля UnemployedMen в том месяце, когда было меньше всего вызовов в Центральном административном округе.'''
+
+import pandas as pd
+
+data1 = pd.read_csv('https://video.ittensive.com/python-advanced/data-9753-2019-07-25.utf.csv', delimiter=';')
+data1 = data1.set_index(['Year', 'Period'])
+data2 = pd.read_csv('https://video.ittensive.com/python-advanced/data-5283-2019-10-04.utf.csv', delimiter=';')
+data2 = data2.set_index(['AdmArea', 'Year', 'Month'])
+data2 = data2.loc['Центральный административный округ']
+data2.index.names = ['Year', 'Period']
+data = pd.merge(data1, data2, left_index=True, right_index=True)
+data = data.reset_index()
+data = data.set_index('Calls')
+data = data.sort_index()
+
+print('Значение поля UnemployedMen, когда было меньше всего вызовов в Центральном административном округе -',
+      int(data['UnemployedMen'][:1].values))
